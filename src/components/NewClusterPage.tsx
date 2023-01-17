6d5c6959195032ca0d62eeab0ec215fc7d418820
Marek Libra <marek.libra@gmail.com>
2023-01-17 13:01:29 +0100

    Change application title

--- src/components/NewClusterPage.tsx
+++ src/components/NewClusterPage.tsx
@@ -3,9 +3,7 @@ import { NewSingleClusterPage } from 'openshift-assisted-ui-lib/ocm';
 import { AppPage } from './AppPage';
 
 // TODO(mlibra): Remove OCP version dropdown
-// TODO(mlibra): Clarify DHCP vs. Static IPs configuration
-// hide operators
-// remove Ad host
+// clarify "Hosts not showing up?"
 // remove SNO
 // final restart-modal
 
